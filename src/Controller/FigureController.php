2775c36ae98c847302be18055fd7c29079ec8ccd
Abou Diallo <abou.diallo78@gmail.com>
2024-03-04 18:44:02 +0100

    created home page

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\FigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FigureController extends AbstractController
 {
-    #[Route('/', name: 'home_figure')]
-    public function home(): Response
+    #[Route('/', name: 'home_home')]
+    public function home(figureRepository $figureRepository): Response
     {
+        $figures = $figureRepository->findby([]);
+
         return $this->render('figure/home.html.twig', [
-            'controller_name' => 'FigureController',
+            "figures" => $figures,
         ]);
     }
+
+
 }
