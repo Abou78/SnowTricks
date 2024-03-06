23384a6c81b2d3746321ac04c75bd06a2d8c5b64
Abou Diallo <abou.diallo78@gmail.com>
2024-03-06 15:06:52 +0100

    add page create

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -2,20 +2,37 @@
 
 namespace App\Controller;
 
-use App\Repository\FigureRepository;
+use App\Entity\Figure;
+use App\Form\FigureType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FigureController extends AbstractController
 {
-    #[Route('/', name: 'home_home')]
-    public function home(figureRepository $figureRepository): Response
+    #[Route('/create', name: 'create_home')]
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager): Response
     {
-        $figures = $figureRepository->findby([]);
+        $figure = new Figure();
+        $figure->setCreatedAt(new \DateTimeImmutable());
+        $figureForm = $this->createForm(FigureType::class, $figure);
 
-        return $this->render('figure/home.html.twig', [
-            "figures" => $figures,
+        $figureForm->handleRequest($request);
+        if ($figureForm->isSubmitted() and $figureForm->isValid()){
+            $entityManager->persist($figure);
+            $entityManager->flush();
+
+            $this->addFlash('figure', 'Votre figure a bien été ajoutée !');
+            return $this->redirectToRoute('home_home');
+        }
+
+        return $this->render('figure/create.html.twig', [
+            'figureForm' => $figureForm->createView(),
         ]);
     }
 
