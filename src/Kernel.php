1b3c20c3d6bd5dc12dff77a5ad26d7e34d1590aa
Abou Diallo <abou.diallo78@gmail.com>
2024-05-01 11:09:42 +0200

    endProject

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,15 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /*// app/AppKernel.php
+    public function registerBundles()
+    {
+        return [
+            // ...
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            // ...
+        ];
+    }*/
 }
+
